e147fd846e994710795666a0cd65e46c86a98954
Aditya Sharma <aadisharma.in+github@gmail.com>
2026-09-20 14:03:33 +0530

    Core Node Created

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-cd /home/$USER/soundmapper || exit 1
+WORK_DIR=$(pwd)
+cd $WORK_DIR || exit 1
 
 if [[ "$1" == "-I" ]];then
     echo "-- Installing Dependecies"
     sudo apt install cmake libglfw3-dev libgl1-mesa-dev pkg-config
 fi
 
-cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release
-cmake --build build -j$(nproc)
+cmake -S . -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_BUILD_TYPE=Release || exit 1
+cmake --build build -j$(nproc) || exit 1
 
 if [[ "$1" == "-R" ]]; then
     echo "-- Running SoundMapper"
-    cd /home/$USER/soundmapper/build/bin
+    cd $WORK_DIR/build/bin || exit 1
     ./soundmapper
+    cd $WORK_DIR || exit 1
 fi
